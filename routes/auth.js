b3422826cec19dadb6c2eed4277bb2a303746960
Maximiliano Zapata <maxzap@gmail.com>
2022-11-25 00:22:30 -0300

    add controllers directory and implements functions

--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,13 @@
 const { Router } = require('express');
 const router = Router();
 
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
 
-router.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-});
+router.post('/new', createUser );
+
+router.post('/', loginUser );
+
+router.get('/renew', renewToken );
 
 module.exports = router;
