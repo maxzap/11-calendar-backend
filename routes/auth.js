635e33db79201b0a1dfc56f58c187cdc710d77d4
Maximiliano Zapata <maxzap@gmail.com>
2022-11-26 00:33:15 -0300

    add validations with express validator package.

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,29 @@
  */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 
 
-router.post('/new', createUser );
+router.post(
+    '/new',
+    [//middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    ],
+    createUser
+);
 
-router.post('/', loginUser );
+router.post('/',
+    [//middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+    ],
+    loginUser
+);
 
 router.get('/renew', renewToken );
 
